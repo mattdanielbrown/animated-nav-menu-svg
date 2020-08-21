507eef58c8699b50652947340b6ed1358147689e
Matt Daniel Brown <developer@mattdanielbrown.com>
2020-08-20 23:11:31 -0500

    Updated tests

--- test.js
+++ test.js
@@ -1,13 +1,42 @@
-import test from 'ava';
-import animatedNavMenuSvg from '.';
+'use strict'
 
-test('title', t => {
-	t.throws(() => {
-		animatedNavMenuSvg(123);
-	}, {
-		instanceOf: TypeError,
-		message: 'Expected a string, got number'
-	});
+const test = require('ava')
+const Window = require('window')
+const window = new Window()
 
-	t.is(animatedNavMenuSvg('unicorns'), 'unicorns & rainbows');
-});
+test('my passing test', t => {
+  t.pass()
+})
+
+function createTitle (text, win) {
+  win = win || (typeof window === 'undefined' ? undefined : window)
+  const title = win.document.createElement('h1')
+  title.innerHTML = text
+  return title
+}
+
+function createSvg (text, win) {
+  win = win || (typeof window === 'undefined' ? undefined : window)
+  const svgItem = win.document.createElement('svg')
+  svgItem.innerHTML = text
+  return svgItem
+}
+
+test('enhanced assertions', t => {
+  const a = /foo/
+  const b = 'bar'
+  const c = 'baz'
+  t.assert(a.test(b) || b !== c)
+})
+
+test('Window Test', t => {
+  const someWindow = createTitle('Hello')
+  t.assert(someWindow)
+})
+
+test('SVG Element', t => {
+  const someSvgElement = createSvg('abc')
+  t.assert(someSvgElement)
+})
+
+module.exports = createTitle
